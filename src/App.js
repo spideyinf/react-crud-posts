632d2a52f09c75073664a7d9bbe5d6de8554cfa0
Phuc Hoang <phuc.lai@solislab.com>
2020-02-16 16:45:58 +0700

    Clean up and add bs4 grid css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <div>React CRUD Post Pages</div>
+      <h1>React CRUD Post Pages</h1>
+      .d-flex
     </div>
   )
 }
